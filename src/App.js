bb326a6eaf0be77a6f711506348c2d9336e54639
Ishumita <ishumita.124@gmail.com>
2020-02-25 03:04:54 +0530

    made changes for test to pass

--- src/App.js
+++ src/App.js
@@ -6,10 +6,62 @@ import FormControl from 'react-bootstrap/FormControl'
 
 let marked = require("marked")
 
+const initialMarkdown = `
+### Headers
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+### List
+
+- list item one
+- list item two
+- list item three
+
+### Links
+
+[FreeCodeCamp](https://learn.freecodecamp.org)
+
+[Google](https://www.google.com "World's Most Popular Search Engine")
+
+### Text Decorations
+
+*italic*
+
+**bold**
+
+***bold and italic***
+
+### Images
+
+![alt text](https://image.shutterstock.com/image-photo/pitbull-dog-alway-smile-260nw-629627696.jpg 'Cute Pitbull')
+
+### Blockquote
+
+> To be, or not to be. That is a stupid question.
+
+### Code
+
+\`npm install create-react-app -g\`
+
+\`\`\`
+function addTwoNumbers(a, b) {
+  return a + b
+}
+const name = 'Benjamin'
+const age = 37
+const number = Math.random() * 10
+\`\`\`
+`
+
 class App extends Component {
 
   state={
-    markdown: ""
+    markdown: initialMarkdown
   }
 
   updateMarkdown=(event)=>{
@@ -27,14 +79,13 @@ class App extends Component {
         <div>
           <FormGroup controlId="formControlsTextarea">
             <FormLabel>Markdown Input</FormLabel>
-            <FormControl as="textarea" placeholder="Enter Markdown" value={markdown}
-            onChange={this.updateMarkdown}></FormControl>
+            <FormControl id="editor" as="textarea" value={markdown} onChange={this.updateMarkdown}></FormControl>
          
           </FormGroup>
         </div>
         <div>
           <h1>Markdown Output</h1>
-          <div dangerouslySetInnerHTML = {{__html:(marked(markdown))}}>
+          <div id="preview" dangerouslySetInnerHTML = {{__html:(marked(markdown))}}>
             {/* {marked(markdown)} */}
           </div>
         </div>
